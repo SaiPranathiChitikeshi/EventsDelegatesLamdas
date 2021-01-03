e92b2d919956121ba33d3532df8b3d4c24f66344
SaiPranathiChitikeshi <75035288+SaiPranathiChitikeshi@users.noreply.github.com>
2021-01-03 20:18:06 +0530

    Add files via upload

---
+++ Lamda_expressions/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lambda_exressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            List<int> numbers = new List<int>() {36, 71, 12,
+                             15, 29, 18, 27, 17, 9, 34};
+
+            
+            Console.Write("The list : ");
+            foreach (var value in numbers)
+            {
+                Console.Write("{0} ", value);
+            }
+            Console.WriteLine();
+
+            // Using lambda expression to calculate square of each value in the list 
+            var square = numbers.Select(x => x * x); 
+            Console.Write("Squares : ");
+            foreach (var value in square)
+            {
+                Console.Write("{0} ", value);
+            }
+            Console.WriteLine();
+
+            // Using Lambda exression to find all numbers in the list divisible by 3 
+            List<int> divBy3 = numbers.FindAll(x => (x % 3) == 0);
+            Console.Write("Numbers Divisible by 3 : ");
+            foreach (var value in divBy3)
+            {
+                Console.Write("{0} ", value);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Lamda_expressions_for_strings/Lamda_expressions_for_strings/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lamda_expressions_for_strings
+{
+    class Student
+    {
+
+        
+        public int rollNo
+        {
+            get;
+            set;
+        }
+
+        public string name
+        {
+            get;
+            set;
+        }
+    }
+
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            
+            List<Student> details = new List<Student>() {
+            new Student{ rollNo = 1, name = "pranathi" },
+                new Student{ rollNo = 2, name = "ananya" },
+                new Student{ rollNo = 3, name = "Tina" },
+                new Student{ rollNo = 4, name = "zefron" },
+                new Student { rollNo = 5, name = "Taman" }
+        };
+
+            // To sort the details list based on name of student in acsending order 
+            var newDetails = details.OrderBy(x => x.name);
+
+            foreach (var value in newDetails)
+            {
+                Console.WriteLine(value.rollNo + " " + value.name);
+            }
+        }
+    }
+}
